8b95846a50d0f46afc07efd3bdb7b294799187ae
Kenneth Carter <kxccarter@gmail.com>
2020-07-15 13:32:11 -0500

    Got Last Run to update after submit.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
-import RunTracker from '../RunTracker/RunTracker';
+//import RunTracker from '../RunTracker/RunTracker';
 
 class Form extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      lastRun: 'No Data',
+      longestRun: 'No Data',
+    };
   }
 
-  handleForm = (event) => {
+  addRun = (event) => {
     event.preventDefault();
-    console.log(`We're handling it, okay?`);
+    console.log(`in addRun`);
+    this.setState({
+      lastRun: this.state.currentRun,
+    });
+    console.log(this.state.lastRun);
   };
 
   handleChange = (event) => {
-    console.log(event.target.value);
+    this.setState({
+      currentRun: event.target.value,
+    });
+
+    console.log(this.state.lastRun);
   };
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleForm}>
-          <input placeholder="Miles Ran" onChange={this.handleChange}></input>
+        <form onSubmit={this.addRun}>
+          <input type="text" required placeholder="Miles Ran" onChange={this.handleChange} />
           <button>Add New Run</button>
         </form>
-        <RunTracker />
+
+        <p>Last Run: {this.state.lastRun}</p>
+        <p>Longest Run: {this.state.longestRun}</p>
+        {/* <RunTracker value={this.state.lastRun} recent={this.state} longest="null" /> */}
       </div>
     );
   }
--- src/components/RunTracker/RunTracker.js
+++ src/components/RunTracker/RunTracker.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 class RunTracker extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recent: this.props.value,
+    };
+  }
+
   render() {
     return (
       <div>
-        <p>Recent Run: {}</p>
-        <p>Longest Run: {}</p>
+        <p>Recent Run: {this.props.value}</p>
+        <p>Longest Run: {this.props.recent.runLog}</p>
       </div>
     );
   }
